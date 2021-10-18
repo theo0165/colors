c341937c96cfb02efc2a38e90844291ee42c3c94
Theo Sandell <theo01sandell@gmail.com>
2021-10-18 16:19:50 +0200

    Styled 404 page

---
+++ components/404.php
@@ -0,0 +1,9 @@
+<?php
+http_response_code(404);
+?>
+<link rel="stylesheet" href="/public/style/css/404.css">
+<div class="not-found">
+    <span>ಠ_ಠ</span>
+    <h1>404 page not found</h1>
+    <a href="/"><button>Go Back</button></a>
+</div>
