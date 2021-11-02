a80d1a7a4f6aeaad6b4bcb7c954e8355445f427d
Theo Sandell <theo01sandell@gmail.com>
2021-11-02 13:01:46 +0100

    Fixed like api endpoint, script now dies correctly

--- api/post/like.php
+++ api/post/like.php
@@ -7,12 +7,14 @@ require __DIR__ . '/../../db.php';
 require __DIR__ . '/../../functions.php';
 
 if ($_SERVER['REQUEST_METHOD'] != "POST") {
+    http_response_code(405);
     header("Location: " . $settings['site_url'] . "/");
+    exit;
 }
 
 if (!isset($_POST['id']) || empty($_POST['id'])) {
     http_response_code(400);
-    exit();
+    exit;
 }
 
 // ALL CHECKS PASSED
@@ -24,8 +26,8 @@ $ip = $_SERVER['REMOTE_ADDR'];
 if (!checkIfIpLikedPost($database->getDb(), $postId, $ip)) {
     http_response_code(201);
     likePost($database->getDb(), $postId, $ip);
-    exit();
+    exit;
 } else {
     http_response_code(403);
-    exit();
+    exit;
 }
